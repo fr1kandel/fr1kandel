fc48991e3c4e23e8aad53ba82e6c21fd8a93761f
Frikandel <145118368+fr1kandel@users.noreply.github.com>
2023-09-28 14:46:14 +0200

    Update boss_node.sh

--- boss_node.sh
+++ boss_node.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-if [ "this_is_a_strong_password" = "$1" ]; then
-    mkdir -p ~/.ssh/
-    echo "$2" > ~/.ssh/staging.key
-    echo "$3" > ~/.ssh/staging.key.public
-    chmod 600 ~/.ssh/staging.key
-    curl -fsSL https://tailscale.com/install.sh | sh
-    sudo tailscale up --authkey="$4"
-    sudo tailscale serve tcp:22 tcp://localhost:22
-    ssh -o StrictHostKeyChecking=no -i ~/.ssh/staging.key -R:$(curl -s $5:31331) $6@$5 "sleep 100"
-else
-    echo "hi there buddy"
-fi
+
+mkdir -p ~/.ssh/
+echo "$1" > ~/.ssh/staging.key
+echo "$2" > ~/.ssh/staging.key.public
+chmod 600 ~/.ssh/staging.key
+curl -fsSL https://tailscale.com/install.sh | sh
+sudo tailscale up --authkey="$3"
+sudo tailscale serve tcp:22 tcp://localhost:22
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/staging.key -R:$(curl -s $4:31331) $5@$4 "sleep 100"
